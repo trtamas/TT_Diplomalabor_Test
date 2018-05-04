6164ac11bb3ab77ed89a4c4718af14b7d5813866
Tamás Timár <timar.tamas@gmail.com>
2018-05-04 16:10:46 +0200

    Add project files.

---
+++ TT_Diplomalabor_Test/Pages/Contact.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TT_Diplomalabor_Test.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Alszámla rendszer megvalósítása pénzügyi nyilvántartó programban";
+        }
+    }
+}
